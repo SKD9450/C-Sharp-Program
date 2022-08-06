c545b492dbcea8f85e5a215be87f7dbb3e20fb50
Shivadatt Dubey <89791382+SKD9450@users.noreply.github.com>
2022-08-06 16:40:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Pattern
+{
+    static void cas(int num)
+    {
+        int i = 1;
+       
+       while(i <= num)
+       {
+            int j = 1;
+            while (j<=i)
+            {
+                j++;
+                Console.Write("*");
+
+            }
+            i++;
+            Console.WriteLine();
+       }
+    }
+    static void Main()
+    {
+        Console.Write("Enter the no:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        cas(num);
+        Console.ReadKey();
+    }
+}
